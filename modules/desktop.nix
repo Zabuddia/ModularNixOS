02b0721417256a563d9a24d23a1f74815490f1a7
Alan Fife <fife.alan@gmail.com>
2025-09-09 16:51:29 -0600

    Got rid of 'minimal' setups

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -1,51 +1,33 @@
 { lib, config, pkgs, hostDesktop, ... }:
 let
   d = hostDesktop;
-
-  # Optional presets (empty set if not selected)
-  gnomePreset =
-    if d == "gnome-minimal"
-    then import ./desktops/gnome/minimal.nix { inherit pkgs; }
-    else {};
-
-  plasmaPreset =
-    if d == "plasma-minimal"
-    then import ./desktops/plasma/minimal.nix { inherit pkgs; }
-    else {};
 in
 lib.mkMerge [
-  # GNOME (full + minimal both enable GNOME)
-  (lib.mkIf (d == "gnome" || d == "gnome-minimal") {
+  (lib.mkIf (d == "gnome") {
     services.xserver.enable = true;
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.desktopManager.gnome.enable = true;
   })
-  gnomePreset
 
-  # Plasma (full + minimal both enable Plasma 6)
-  (lib.mkIf (d == "plasma" || d == "plasma-minimal") {
+  (lib.mkIf (d == "plasma") {
     services.xserver.enable = true;
     services.displayManager.sddm.enable = true;
     services.displayManager.sddm.wayland.enable = true;
     services.desktopManager.plasma6.enable = true;
   })
-  plasmaPreset
 
-  # Cinnamon
   (lib.mkIf (d == "cinnamon") {
     services.xserver.enable = true;
     services.xserver.displayManager.lightdm.enable = true;
     services.xserver.desktopManager.cinnamon.enable = true;
   })
 
-  # Pantheon
   (lib.mkIf (d == "pantheon") {
     services.xserver.enable = true;
     services.xserver.displayManager.lightdm.enable = true;
     services.xserver.desktopManager.pantheon.enable = true;
   })
 
-  # Headless
   (lib.mkIf (d == "headless") {
     services.xserver.enable = lib.mkForce false;
     services.xserver.displayManager.gdm.enable = lib.mkForce false;
--- modules/desktops/plasma/minimal.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs }:
-{
-  environment.plasma6.excludePackages = with pkgs.kdePackages; [
-    discover elisa kcalc kcharselect konversation kweather
-    kdepim-addons okular gwenview kmail kaddressbook
-  ];
-}
