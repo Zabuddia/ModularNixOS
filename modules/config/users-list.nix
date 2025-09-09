096e621264d873fad752e2bd60d51f6354e7d6ad
Alan Fife <fife.alan@gmail.com>
2025-09-09 17:44:34 -0600

    Updated email

--- modules/config/users-list.nix
+++ modules/config/users-list.nix
@@ -3,7 +3,7 @@
     {
       name = "buddia";
       fullName = "Alan Fife";
-      email = "fife.alan@gmail.com";
+      email = "fife.alan@protonmail.com";
       hashedPassword = "$y$j9T$I8EQYRnAKlWsvquySBpRE1$7fyHAZ/84X2fY1FiX7TVavbtn2FB0/15HsSUBSTgM9A";
       groups = [ "wheel" "networkmanager" ];
       shell = "bash";
