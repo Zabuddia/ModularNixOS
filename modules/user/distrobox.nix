c1086e645381772d60f126c67dc330aaafffb9a2
Alan Fife <fife.alan@protonmail.com>
2025-09-16 16:12:59 -0600

    added to distrobox

--- modules/user/distrobox.nix
+++ modules/user/distrobox.nix
@@ -3,5 +3,15 @@
   programs.distrobox = {
     enable = true;
     package = pkgs.distrobox;
+
+    # Automatically keep containers in sync with your HM config
+    enableSystemdUnit = true;
+
+    # Example container definition
+    containers.ubuntu = {
+      image = "docker.io/library/ubuntu:22.04";
+      init = true;  # boot a simple init process inside the container
+      additionalPackages = [ "git" "curl" ]; # install extra tools
+    };
   };
 }
