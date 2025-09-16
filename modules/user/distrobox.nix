3f2d2ae42a722408a8054c6be61cbed889a5e2d8
Alan Fife <fife.alan@protonmail.com>
2025-09-16 16:28:07 -0600

    other distrobox stuff wasn't working so I simplified it

--- modules/user/distrobox.nix
+++ modules/user/distrobox.nix
@@ -3,15 +3,5 @@
   programs.distrobox = {
     enable = true;
     package = pkgs.distrobox;
-
-    # Automatically keep containers in sync with your HM config
-    enableSystemdUnit = true;
-
-    # Example container definition
-    containers.ubuntu = {
-      image = "docker.io/library/ubuntu:22.04";
-      init = false;
-      additionalPackages = [ "git" "curl" ]; # install extra tools
-    };
   };
 }
