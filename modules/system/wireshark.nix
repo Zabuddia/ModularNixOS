8856eabda87e07ec807f352228ec4e9aeaecd322
Alan Fife <fife.alan@protonmail.com>
2025-09-16 15:14:12 -0600

    Updated wireshark

--- modules/system/wireshark.nix
+++ modules/system/wireshark.nix
@@ -5,15 +5,19 @@ let
   userNames = map (u: u.name) (userList.users or []);
 in
 {
-  programs.wireshark = {
-    enable = true;
-    package = pkgs.wireshark;  # use pkgs.wireshark-cli if you only want TUI
-    dumpcap.enable = true;     # capture as non-root
-    usbmon.enable = false;
-  };
+  config = lib.mkMerge [
+    {
+      programs.wireshark = {
+        enable = true;
+        package = pkgs.wireshark;  # use pkgs.wireshark-cli if you only need TUI
+        dumpcap.enable = true;     # capture as non-root
+        usbmon.enable = false;
+      };
+    }
 
-  # Append "wireshark" to each user from users-list.nix
-  config = lib.mkIf (userNames != []) (lib.mkMerge (map
-    (n: { users.users.${n}.extraGroups = lib.mkAfter [ "wireshark" ]; })
-    userNames));
+    # Append "wireshark" to each user from users-list.nix
+    (lib.mkIf (userNames != []) (lib.mkMerge (map
+      (n: { users.users.${n}.extraGroups = lib.mkAfter [ "wireshark" ]; })
+      userNames)))
+  ];
 }
