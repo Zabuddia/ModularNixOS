d92a673213732608b53874d460b94620bc337165
Alan Fife <fife.alan@protonmail.com>
2025-09-16 16:20:11 -0600

    updated distrobox

--- modules/user/distrobox.nix
+++ modules/user/distrobox.nix
@@ -10,7 +10,6 @@
     # Example container definition
     containers.ubuntu = {
       image = "docker.io/library/ubuntu:22.04";
-      init = true;  # boot a simple init process inside the container
       additionalPackages = [ "git" "curl" ]; # install extra tools
     };
   };
