1643ed517f618b0bd6ac199dd2199c67eb7e331a
Alan Fife <fife.alan@protonmail.com>
2025-09-24 16:54:44 -0600

    more testing

--- modules/config/hosts.nix
+++ modules/config/hosts.nix
@@ -6,7 +6,8 @@
       desktop = "gnome";
       services = [
         { name = "gitea"; port = 3000; expose = "caddy"; scheme = "https"; domain = "alan-mba-2013"; }
-        { name = "pyhttp"; port = 3001; expose = "caddy"; scheme = "http"; domain = "alan-mba-2013"; }
+        { name = "pyhttp"; port = 3001; expose = "caddy"; scheme = "https"; domain = "alan-mba-2013"; }
+        { name = "invidious"; port = 3002; expose = "tailscale"; scheme = "https"; domain = "alan-mba-2013"; }
       ];
       modules = [
         ../../hosts/alan-mba-2013-hardware.nix
