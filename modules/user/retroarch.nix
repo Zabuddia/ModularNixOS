717f0d266305a7682978900bba6c7223e04732c3
Alan Fife <fife.alan@protonmail.com>
2025-09-26 18:28:35 -0600

    Trying to get retroarch to work

--- modules/user/retroarch.nix
+++ modules/user/retroarch.nix
@@ -3,6 +3,14 @@
 {
   home.packages = with pkgs; [
     retroarch
-    retroarchCores.dolphin
+    libretro-core-info
+    retroarch-assets
+
+    # Cores to include
+    libretro.dolphin
   ];
+
+  # Symlink the Dolphin core where RetroArch expects it
+  home.file.".config/retroarch/cores/dolphin_libretro.so".source =
+    "${pkgs.libretro.dolphin}/lib/libretro/dolphin_libretro.so";
 }
