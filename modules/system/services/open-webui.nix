207e233839b1f332335c7b88e8f6fdfacf3b5ec3
Alan Fife <fife.alan@protonmail.com>
2025-09-29 11:21:34 -0600

    Updated open-webui.nix

--- modules/system/services/open-webui.nix
+++ modules/system/services/open-webui.nix
@@ -14,7 +14,10 @@
       ANONYMIZED_TELEMETRY = "False";
       DO_NOT_TRACK = "True";
       SCARF_NO_ANALYTICS = "True";
-      WEBUI_AUTH = "False";
+      WEBUI_AUTH = "True";
+      ENABLE_SIGNUP = "False";             # disable self-registration
+      DEFAULT_USER_ROLE = "pending";
+      WEBUI_URL = "${scheme}://${host}:${toString port}";
       # OPENAI_API_BASE_URL = "http://localhost:8000/v1";
       # OPENAI_API_BASE_URL = "http://localhost:8001/v1";
       # OLLAMA_BASE_URL = "http://localhost:11434";
