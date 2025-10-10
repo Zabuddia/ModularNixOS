b3c00227b3d0fe8340957820588cfcfa27c4f183
Alan Fife <fife.alan@protonmail.com>
2025-10-10 14:20:02 -0600

    another bug fix

--- modules/system/ddns-cloudflare.nix
+++ modules/system/ddns-cloudflare.nix
@@ -14,23 +14,18 @@ in
       message = "inadyn: No FQDNs found in hostServices (.host or .domain)." ; }
   ];
 
-  # (Optional) seed the token (prefer sops/agenix in prod)
-  # environment.etc."cloudflare-ddns-token".text = "<YOUR_CF_API_TOKEN>";
-  # environment.etc."cloudflare-ddns-token".mode = "0600";
-
   services.inadyn = {
     enable = true;
     settings = {
       period = 300;
       web = "ipify";
-      # ipv6 = false;  # uncomment if you want to force IPv4-only
+      # ipv6 = false;  # uncomment to force IPv4-only
 
       provider = {
-        cloudflare = {
-          system     = "cloudflare.com";
-          username = "token";
-          password = passwordPath;
-          hostname = uniqFqdns;
+        "cloudflare.com" = {
+          username = "token";          # literal word "token"
+          password = passwordPath;     # file with your API token
+          hostname = uniqFqdns;        # all FQDNs you collected
         };
       };
     };
