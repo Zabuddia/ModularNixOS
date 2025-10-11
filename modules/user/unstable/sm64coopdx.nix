e3277da5f2c991beef513ced7882f9336225e64d
waffleiron <fife.randy@protonmail.com>
2025-10-11 14:47:41 -0400

    Added instructions for sm64coopdx

--- modules/user/unstable/sm64coopdx.nix
+++ modules/user/unstable/sm64coopdx.nix
@@ -1,3 +1,5 @@
+# nix store add-file --hash-type sha256 baserom.us.z64
+# or sudo nix-store --add-fixed sha256 baserom.us.z64
 { unstablePkgs, ... }:
 {
   home.packages = [ unstablePkgs.sm64coopdx ];
