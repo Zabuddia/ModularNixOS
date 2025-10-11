47ed16fd0f6a6af0648e325222632725cfd4ba21
waffleiron <fife.randy@protonmail.com>
2025-10-11 14:39:53 -0400

    Updated gitea to work better with the ports

--- modules/system/services/gitea.nix
+++ modules/system/services/gitea.nix
@@ -1,14 +1,29 @@
 { scheme, host, port, lanPort, streamPort, expose, edgePort }:
 
 { config, lib, pkgs, ... }:
+let
+  inherit (lib) mkIf;
+  extPort =
+    if expose == "caddy-wan" then edgePort else lanPort;
+
+  extPortSuffix =
+    if extPort == null || extPort == 80 || extPort == 443 then "" else ":${toString extPort}";
+
+  externalURL = "${scheme}://${host}${extPortSuffix}";
+in
 {
   services.gitea = {
     enable = true;
+    package = pkgs.gitea;
+
     settings.server = {
       HTTP_ADDR = "127.0.0.1";
-      DOMAIN = host;
-      ROOT_URL = "${scheme}://${host}/";
       HTTP_PORT = port;
+      DOMAIN = host;
+      ROOT_URL = externalURL + "/";
     };
+
+    # Optional: if you want repositories stored somewhere specific
+    # settings.repository.ROOT = "/var/lib/gitea/data/repositories";
   };
 }
