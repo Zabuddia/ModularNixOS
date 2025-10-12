7ad93d039aee864ee7e217535264b6bc741160f8
Alan Fife <fife.alan@protonmail.com>
2025-10-12 10:04:56 -0600

    added a stable sm64coopdx.nix

---
+++ modules/user/sm64coopdx.nix
@@ -0,0 +1,15 @@
+# nix store add-file --hash-type sha256 baserom.us.z64
+# or sudo nix-store --add-fixed sha256 baserom.us.z64
+{ pkgs, ... }:
+{
+  home.packages = [ pkgs.sm64coopdx ];
+
+  xdg.desktopEntries.sm64coopdx = {
+    name = "Super Mario 64 CoopDX";
+    comment = "Launch SM64 Cooperative DX";
+    exec = "sm64coopdx";
+    icon = "sm64coopdx";
+    terminal = false;
+    categories = [ "Game" ];
+  };
+}
