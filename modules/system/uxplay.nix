52e016e522e5e6e49d92e7659bc325b512ea58dd
Alan Fife <fife.alan@protonmail.com>
2025-11-02 20:49:13 -0700

    Made a uxplay service

--- modules/system/uxplay.nix
+++ modules/system/uxplay.nix
@@ -1,9 +1,11 @@
-{ config, pkgs, ... }:
+{ config, unstablePkgs, ... }:
 
 {
   # Open network ports
-#   networking.firewall.allowedTCPPorts = [ 7000 7001 7100 ];
-#   networking.firewall.allowedUDPPorts = [ 5353 6000 6001 7011 ];
+  networking.firewall = {
+    allowedTCPPorts = [ 7000 7001 7100 ];
+    allowedUDPPorts = [ 6000 6001 7011 ];
+  };
 
 #   # To enable network-discovery
 #   services.avahi = {
@@ -19,8 +21,16 @@
 #     };
 #   };
 
-  environment.systemPackages = with pkgs; [
-    uxplay
-  ];
+  environment.systemPackages = with unstablePkgs; [ uxplay ];
 
+  # Optional: a simple user service so it always runs with the right flags
+  systemd.user.services.uxplay = {
+    description = "UxPlay AirPlay receiver";
+    after       = [ "graphical-session.target" "avahi-daemon.service" ];
+    wantedBy    = [ "graphical-session.target" "default.target" ];
+    serviceConfig = {
+      ExecStart = "${unstablePkgs.uxplay}/bin/uxplay -p -fs";
+      Restart   = "on-failure";
+    };
+  };
 }
