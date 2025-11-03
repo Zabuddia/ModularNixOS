fe6a678a0bcbef1b21763be82e528210d3397df2
Alan Fife <fife.alan@protonmail.com>
2025-11-03 15:46:18 -0700

    updated boot-into-kodi.nix

--- modules/user/boot-into-kodi.nix
+++ modules/user/boot-into-kodi.nix
@@ -1,6 +1,23 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.programs.kodi;
+in
 {
-  xsession.initExtra = ''
-    kodi &
-  '';
+  assertions = [{
+    assertion = cfg.enable;
+    message = "boot-into-kodi.nix: Please import kodi.nix first.";
+  }];
+
+  systemd.user.services.kodi-autostart = {
+    Unit = {
+      Description = "Autostart Kodi on graphical login";
+      After = [ "graphical-session.target" ];
+      PartOf = [ "graphical-session.target" ];
+    };
+    Service = {
+      ExecStart = "${cfg.package}/bin/kodi --fullscreen";
+      Restart = "no";
+    };
+    Install.WantedBy = [ "graphical-session.target" ];
+  };
 }
