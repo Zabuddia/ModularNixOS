0eb8bc629a86b5d2d68f095cf87f8f36010f90f4
Alan Fife <fife.alan@protonmail.com>
2025-11-03 20:30:29 -0700

    still trying to figure out tor

--- modules/system/tor.nix
+++ modules/system/tor.nix
@@ -1,4 +1,16 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+
 {
-  services.tor.enable = true;
+  services.tor = {
+    enable = true;
+    # client.enable = true;  # prevents SocksPort 0 from being injected
+
+    # settings = {
+    #   # IMPORTANT: force a single SocksPort, no flags, no duplicates
+    #   SocksPort = lib.mkForce [ "127.0.0.1:9052" ];
+    #   # Keep it minimal until we're green; add ControlPort later if you want
+    # };
+  };
+
+  environment.systemPackages = with pkgs; [ torsocks ];
 }
