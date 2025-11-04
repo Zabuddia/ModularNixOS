133a8a2e2fa5b5fc14944f447d01b4682fd50bbf
waffleiron <fife.randy@protonmail.com>
2025-11-04 15:09:54 -0500

    jellyfin works but I can't figure out how to change the port

--- modules/system/services/jellyfin.nix
+++ modules/system/services/jellyfin.nix
@@ -1,15 +1,10 @@
 { scheme, host, port, lanPort, streamPort, expose, edgePort }:
 
-{ pkgs, ... }:
+{ config, pkgs, lib, ... }:
 {
   services.jellyfin = {
     enable = true;
     package = pkgs.jellyfin;
     openFirewall = false;
   };
-
-  # Force Jellyfin to use your chosen port
-  systemd.services.jellyfin.serviceConfig.ExecStart = [
-    "${pkgs.jellyfin}/bin/jellyfin --httpport ${toString port}"
-  ];
 }
