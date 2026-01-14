89a12de7ae8ac8dad88c6c75cadcb816a98813a7
Alan Fife <fife.alan@protonmail.com>
2026-01-14 09:40:47 -0700

    updated wireguard.nix

--- modules/system/wireguard.nix
+++ modules/system/wireguard.nix
@@ -6,13 +6,6 @@
 
   # Userspace tools (wg, wg-quick)
   environment.systemPackages = with pkgs; [
-    wireguard
     wireguard-tools
-  ];
-
-  # If you *specifically* want the kernel module package (older kernels / special cases),
-  # keep this. It's usually unnecessary on recent kernels, but harmless.
-  boot.extraModulePackages = [
-    config.boot.kernelPackages.wireguard
   ];
 }
