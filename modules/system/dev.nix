ec7af3b0d34bedd67cd13d51df8dee86fb3fe0c1
Alan Fife <fife.alan@protonmail.com>
2026-01-26 17:47:49 -0700

    Added stuff to do infinisim

--- modules/system/dev.nix
+++ modules/system/dev.nix
@@ -5,12 +5,14 @@
     gdb valgrind strace ltrace clang-tools
     gtest
 
-    python3
-    python3Packages.pipx
-    python3Packages.click
-    python3Packages.cryptography
-    python3Packages.cbor
-    python3Packages.intelhex
+    (python3.withPackages (ps: with ps; [
+      pillow
+      click
+      cryptography
+      cbor
+      intelhex
+    ]))
+    
     gcc-arm-embedded
     openocd
     rustup
@@ -21,6 +23,12 @@
     uv
 
     imagemagick patchelf
+
+    SDL2 (lib.getDev SDL2)
+    SDL2_ttf (lib.getDev SDL2_ttf)
+    freetype (lib.getDev freetype)
+    zlib (lib.getDev zlib)
+    libpng (lib.getDev libpng)
   ];
 
   environment.variables.JAVA_HOME = pkgs.jdk.home;
